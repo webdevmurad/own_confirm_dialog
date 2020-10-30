cbeadf75e959b965429a0396e3f565f3bcb23b26
Murad <murad-arslangiraev-1994@mail.ru>
2020-10-30 16:25:41 +0300

    Updates

---
+++ precache-manifest.48dff4c0991169f0463a06f014fbe21c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a590e5ae3c1aae5ff645702379451afd",
+    "url": "/own_confirm_dialog/index.html"
+  },
+  {
+    "revision": "07e65163f4e2afa5513f",
+    "url": "/own_confirm_dialog/static/css/main.b30e35bf.chunk.css"
+  },
+  {
+    "revision": "4c9afcc1d6f5194ba42f",
+    "url": "/own_confirm_dialog/static/js/2.52fbeb5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/own_confirm_dialog/static/js/2.52fbeb5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e65163f4e2afa5513f",
+    "url": "/own_confirm_dialog/static/js/main.0e464ddf.chunk.js"
+  },
+  {
+    "revision": "127e1b49e3aea0dd6a4a",
+    "url": "/own_confirm_dialog/static/js/runtime-main.bce3963a.js"
+  }
+]);
